00c3ebb19d96f3f91f3f70be42ab3fbc23a71ace
toureda <felipefonseca.n99@gmail.com>
2024-12-10 15:15:01 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+export default defineConfig({
+  site: "https://toureda.netlify.app/",
+  integrations: [preact()]
+});
--- src/pages/posts/post-4.md
+++ src/pages/posts/post-4.md
@@ -1,7 +1,7 @@
 ---
 layout: ../../layouts/MarkdownPostLayout.astro
 title: Mi cuarta entrada en el blog
-author: Felipe Nuñez Fonseca
+author: Felipe Nuñez
 description: "Esta entrada aparecerá sola!"
 image:
   url: "https://docs.astro.build/default-og-image.png"
